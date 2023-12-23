c3c79a406faa7a0e8e3de779bcd889d788f4bc0e
KevalKThumar <thumarkeval12@gmail.com>
2023-12-23 14:39:10 +0530

    display joke

--- jsProject/13-jokes/chaiaurcode.js
+++ jsProject/13-jokes/chaiaurcode.js
@@ -1,8 +1,30 @@
 const url = 'https://api.chucknorris.io/jokes/random';
+const btn = document.querySelector('.btn');
 
 // handle this end point with XMLHttpRequest
 
-// handle this end point with promises
 
 // handle the case of race condition
+
+const xMLHttpRequest = new XMLHttpRequest();
+btn.addEventListener('click', function () {
+    xMLHttpRequest.open('GET', url);
+    xMLHttpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.response);
+            document.querySelector('#display-joke').innerHTML = data.value;
+        }
+    }
+    xMLHttpRequest.send();
+})
+
+// handle this end point with promises
+
+// btn.addEventListener('click', () => {
+//     fetch(url)
+//         .then((res) => res.json()).then((data) => {
+//             document.querySelector('#display-joke').innerHTML = data.value
+//         })
+// })
+
 
